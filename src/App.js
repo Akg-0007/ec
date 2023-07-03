100d078e88060f4e73ce7b64637505f450d7e8f0
akash.gupta3203707 <akash.gupta@venturepact.com>
2023-07-03 08:46:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+
+import './App.css';
+import ProductList from './features/product-list/productList';
+import Cart from './features/cart/Cart';
+import Home from './pages/Home';
+import Loginpage from './pages/Loginpage';
+import Signuppage from './pages/Signuppage';
+
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>
+  },
+  {
+    path: "login",
+    element: <Loginpage></Loginpage>
+  },
+  {
+    path: "signup",
+    element: <Signuppage></Signuppage>
+  },
+  {
+    path: "cart",
+    element: <Cart></Cart>
+  },
+]);
+
+
+function App() {
+  return (
+    <div className="App">
+     {/* <ProductList/> */}
+     {/* <Home/> */}
+     {/* <Loginpage></Loginpage> */}
+     {/* <Signuppage></Signuppage>
+      */}
+        <RouterProvider router={router} />
+
+    </div>
+  );
+}
+
+export default App;
