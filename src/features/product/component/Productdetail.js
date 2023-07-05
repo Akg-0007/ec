aa90b36d2af92d215aaa362716f2b78ec8e34355
akash.gupta3203707 <akash.gupta@venturepact.com>
2023-07-05 11:51:27 +0530

    fix

--- src/features/product/component/Productdetail.js
+++ src/features/product/component/Productdetail.js
@@ -273,7 +273,7 @@ const Productdetail = () => {
           </div>
 
           <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
-            {/* Description and details */}
+            
             <div>
               <h3 className="sr-only">Description</h3>
 
